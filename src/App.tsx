0e0e13ff1754278eb537c6336b1ebd23119a320e
ShadowCatP <282276@student.pwr.edu.pl>
2025-09-22 23:51:47 +0200

    feat: initial grid split

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,13 @@ import { Weather } from "./components/widgets/Weather/Weather";
 
 export const App = () => {
   return (
-    <div className="grid min-h-screen w-full place-content-center">
-      <Clock />
-      <Weather />
-      <Notes />
-      <Links />
+    <div className="mx-auto grid w-full place-content-center px-2">
+      <div className="grid min-h-screen w-full grid-cols-3 place-content-center gap-3">
+        <Clock />
+        <Weather />
+        <Notes />
+        <Links />
+      </div>
     </div>
   );
 };
